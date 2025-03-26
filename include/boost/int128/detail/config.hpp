d6046350dea80a9fb8e0f8ba10c9a572bb4abb2c
Matt Borland <matt@mattborland.com>
2025-03-26 15:20:41 -0400

    Add detection for C++20 concepts

--- include/boost/int128/detail/config.hpp
+++ include/boost/int128/detail/config.hpp
@@ -27,4 +27,12 @@
 
 #endif // Determine endianness
 
+// Determine if we can use concepts
+#if (__cplusplus >= 202002L || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L)) &&\
+    (!defined(__GNUC__) || __GNUC__ != 11) && __has_include(<concepts>)
+
+#define BOOST_INT128_HAS_CONCEPTS
+
+#endif
+
 #endif // BOOST_INT128_DETAIL_CONFIG_HPP
