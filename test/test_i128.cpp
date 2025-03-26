9d5ea44da5371ae8ea56cefc116906f59b7864fa
Matt Borland <matt@mattborland.com>
2025-03-26 13:25:59 -0400

    Add arithmetic constructor test

--- test/test_i128.cpp
+++ test/test_i128.cpp
@@ -62,4 +62,63 @@ static std::mt19937_64 rng(42);
 
 constexpr std::size_t N = 1024;
 
+#ifdef BOOST_INT128_HAS_INT128
 
+// We reduce the max end of the 128 bit types as they can cause a stack overflow in boost.random
+
+template <>
+constexpr unsigned __int128 get_max<unsigned __int128>()
+{
+    return static_cast<unsigned __int128>(UINT64_MAX) << 64 | UINT64_MAX / 32;
+}
+
+template <>
+constexpr unsigned __int128 get_min<unsigned __int128>()
+{
+    return 0;
+}
+
+template <>
+constexpr __int128 get_max<__int128>()
+{
+    return static_cast<__int128>((static_cast<unsigned __int128>(1) << 127) - 1) / 32;
+}
+
+template <>
+constexpr __int128 get_min<__int128>()
+{
+    return -get_max<__int128>() - 1;
+}
+
+template <typename IntType>
+void test_arithmetic_constructor()
+{
+    boost::random::uniform_int_distribution<IntType> dist(get_min<IntType>(),
+                                                          get_max<IntType>());
+
+    for (std::size_t i {}; i < N; ++i)
+    {
+        const IntType value {dist(rng)};
+        __int128 builtin_value = static_cast<__int128>(value);
+        boost::int128::int128_t emulated_value {value};
+
+        __int128 emulated_bits;
+        std::memcpy(&emulated_bits, &emulated_value, sizeof(__int128));
+
+        BOOST_TEST(emulated_bits == builtin_value);
+    }
+}
+
+int main()
+{
+    return 0;
+}
+
+#else
+
+int main()
+{
+    return 0;
+}
+
+#endif
