d581ce2a577647dad162d521d940a6083694b738
Matt Borland <matt@mattborland.com>
2025-03-26 14:32:57 -0400

    Part 2 fix architecture

---
+++ include/boost/int128/detail/int128_imp.hpp
@@ -0,0 +1,13 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef INT128_HPP
+#define INT128_HPP
+
+struct int128_t
+{
+
+};
+
+#endif //INT128_HPP
--- test/test_u128.cpp
+++ test/test_u128.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/int128/detail/uint128.hpp>
+#include <boost/int128/detail/uint128_imp.hpp>
 #include <boost/int128/detail/conversions.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <cstring>
