5fd208a5d634bad35c2bdf3f10dab3c2c6022a1e
Matt Borland <matt@mattborland.com>
2025-03-26 15:20:58 -0400

    Make signed and unsigned integers a concept

--- include/boost/int128/detail/traits.hpp
+++ include/boost/int128/detail/traits.hpp
@@ -5,7 +5,12 @@
 #ifndef BOOST_INT128_DETAIL_TRAITS_HPP
 #define BOOST_INT128_DETAIL_TRAITS_HPP
 
+#include <boost/int128/detail/config.hpp>
 #include <type_traits>
+
+#ifdef BOOST_INT128_HAS_CONCEPTS
+#include <concepts>
+#endif
 
 namespace boost {
 namespace int128 {
@@ -29,8 +34,27 @@ struct unsigned_integer
 template <typename T>
 static constexpr bool is_unsigned_integer_v = unsigned_integer<T>::value;
 
+#ifdef BOOST_INT128_HAS_CONCEPTS
+
+concept signed_integer = signed_integer<T>::value;
+concept unsigned_integer = unsigned_integer<T>::value;
+
+#endif
+
 } // namespace detail
 } // namespace int128
 } // namespace boost
+
+#ifdef BOOST_INT128_HAS_CONCEPTS
+
+#define BOOST_INT128_SIGNED_INTEGER_CONCEPT signed_integer SignedInteger
+#define BOOST_INT128_UNSIGNED_INTEGER_CONCEPT unsigned_integer UnsignedInteger
+
+#else
+
+#define BOOST_INT128_SIGNED_INTEGER_CONCEPT typename SignedInteger, std::enable_if_t<detail::is_signed_integer_v<SignedInteger>, bool> = true
+#define BOOST_INT128_UNSIGNED_INTEGER_CONCEPT typename UnsignedInteger, std::enable_if_t<detail::is_unsigned_integer_v<UnsignedInteger>, bool> = true
+
+#endif
 
 #endif // BOOST_INT128_DETAIL_TRAITS_HPP
