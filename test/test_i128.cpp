26ab8cd12db9656c7a9e5406e198a26d1ff8a6bd
Matt Borland <matt@mattborland.com>
2025-03-27 10:34:11 -0400

    Add operator== test

--- test/test_i128.cpp
+++ test/test_i128.cpp
@@ -258,6 +258,39 @@ void test_unary_minus()
     }
 }
 
+template <typename IntType>
+void test_operator_equality()
+{
+    boost::random::uniform_int_distribution<IntType> dist(get_min<IntType>(),
+                                                          get_max<IntType>());
+
+    // Always equal
+    for (std::size_t i {}; i < N; ++i)
+    {
+        const IntType value {dist(rng)};
+        __int128 builtin_value = static_cast<__int128>(value);
+        boost::int128::int128_t emulated_value {value};
+
+        BOOST_TEST(((value == emulated_value) == (emulated_value == value)) ==
+                   ((value == builtin_value) == (builtin_value == value)));
+    }
+
+    // Potentially equal
+    for (std::size_t i {}; i < N; ++i)
+    {
+        const IntType value {dist(rng)};
+        const IntType value2 {dist(rng)};
+        __int128 builtin_value = static_cast<__int128>(value);
+        boost::int128::int128_t emulated_value {value};
+
+        BOOST_TEST(((value2 == emulated_value) == (emulated_value == value2)) ==
+                   ((value2 == builtin_value) == (builtin_value == value2)));
+    }
+
+    const boost::int128::int128_t bool_val {dist(rng)};
+    BOOST_TEST((true == bool_val) == (bool_val == true));
+}
+
 #ifdef _MSC_VER
 #pragma warning(pop)
 #endif
@@ -272,6 +305,7 @@ struct test_caller
         test_integer_conversion_operators<T>();
         test_unary_plus<T>();
         test_unary_minus<T>();
+        test_operator_equality<T>();
     }
 };
 
