9e7b3e5b04ece6e2176bd16c01daa3cbb9921174
Matt Borland <matt@mattborland.com>
2025-04-01 16:15:36 -0400

    Don't run UBSAN on benchmarks

--- test/benchmark_i128.cpp
+++ test/benchmark_i128.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#ifdef NDEBUG
+#if defined(NDEBUG) && !defined(UBSAN)
 #define BOOST_INT128_BENCHMARK_I128
 #endif // NDEBUG
 
