1eb8f148547519c4c8d70096cc1c3883a4e8f8f5
Matt Borland <matt@mattborland.com>
2025-04-15 13:50:53 -0400

    Fix big endian conversion

--- include/boost/int128/detail/conversions.hpp
+++ include/boost/int128/detail/conversions.hpp
@@ -21,7 +21,7 @@ constexpr uint128_t::uint128_t(const int128_t& v) noexcept : low {v.low}, high {
 
 constexpr int128_t::int128_t(const uint128_t& v) noexcept : high {static_cast<std::int64_t>(v.high)}, low {v.low} {}
 
-constexpr uint128_t::uint128_t(const int128_t& v) noexcept : high {static_cast<std::uint64_t>(v.high)}. low {v.low} {}
+constexpr uint128_t::uint128_t(const int128_t& v) noexcept : high {static_cast<std::uint64_t>(v.high)}, low {v.low} {}
 
 #endif // BOOST_INT128_ENDIAN_LITTLE_BYTE
 
