2547872cff6178790ef77755f9b160ddfb6198cb
Matt Borland <matt@mattborland.com>
2025-04-16 13:49:24 -0400

    Optimize operator LE

--- include/boost/int128/detail/uint128_imp.hpp
+++ include/boost/int128/detail/uint128_imp.hpp
@@ -445,7 +445,32 @@ constexpr bool operator<=(const UnsignedInteger lhs, const uint128_t rhs) noexce
 
 constexpr bool operator<=(const uint128_t lhs, const uint128_t rhs) noexcept
 {
+    #if defined(__aarch64__) && defined(__APPLE__) && defined(__clang__)
+
+    return static_cast<detail::builtin_u128>(lhs) < static_cast<detail::builtin_u128>(rhs);
+
+    #elif defined(__x86_64__) && !defined(BOOST_INT128_NO_CONSTEVAL_DETECTION) && defined(__GNUC__) && !defined(__clang__)
+
+    if (BOOST_INT128_IS_CONSTANT_EVALUATED(lhs))
+    {
+        return lhs.high == rhs.high ? lhs.low <= rhs.low : lhs.high <= rhs.high;
+    }
+    else
+    {
+        detail::builtin_u128 builtin_lhs {};
+        detail::builtin_u128 builtin_rhs {};
+
+        std::memcpy(&builtin_lhs, &lhs, sizeof(builtin_lhs));
+        std::memcpy(&builtin_rhs, &rhs, sizeof(builtin_rhs));
+
+        return builtin_lhs < builtin_rhs;
+    }
+
+    #else
+
     return lhs.high == rhs.high ? lhs.low <= rhs.low : lhs.high <= rhs.high;
+
+    #endif
 }
 
 #ifdef BOOST_INT128_HAS_INT128
