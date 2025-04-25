d100b64d56b8c05f3b7afbe9964e31df8c67ec99
Matt Borland <matt@mattborland.com>
2025-04-25 16:12:19 -0400

    Add additional control paths

--- include/boost/int128/detail/uint128_imp.hpp
+++ include/boost/int128/detail/uint128_imp.hpp
@@ -1868,8 +1868,26 @@ constexpr uint128_t operator/(const uint128_t lhs, const uint128_t rhs) noexcept
     {
         return {0, 0};
     }
+    else if (rhs.high != 0)
+    {
+        return detail::knuth_div(lhs, rhs);
+    }
+    else
+    {
+        if (lhs.high == 0)
+        {
+            return {0, lhs.low / rhs.low};
+        }
+        else
+        {
+            uint128_t quotient {};
+            uint128_t remainder {};
 
-    return detail::knuth_div(lhs, rhs);
+            detail::one_word_div(lhs, rhs.low, quotient, remainder);
+
+            return quotient;
+        }
+    }
 }
 
 #ifdef BOOST_INT128_HAS_INT128
