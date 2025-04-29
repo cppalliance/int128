35a03ab39d5b59a21757389341382b362c3502b1
Matt Borland <matt@mattborland.com>
2025-04-29 11:27:39 -0400

    Add specialization of std::numeric_limits

--- include/boost/int128/detail/uint128_imp.hpp
+++ include/boost/int128/detail/uint128_imp.hpp
@@ -1937,4 +1937,47 @@ constexpr uint128_t& uint128_t::operator/=(const uint128_t rhs) noexcept
 } // namespace int128
 } // namespace boost
 
+template <>
+class std::numeric_limits<boost::int128::uint128_t>
+{
+public:
+
+    // Member constants
+    static constexpr bool is_specialized = true;
+    static constexpr bool is_signed = false;
+    static constexpr bool is_integer = true;
+    static constexpr bool is_exact = true;
+    static constexpr bool has_infinity = false;
+    static constexpr bool has_quiet_NaN = false;
+    static constexpr bool has_signaling_NaN = false;
+    static constexpr std::float_denorm_style has_denorm = std::denorm_absent;
+    static constexpr bool has_denorm_loss = false;
+
+    static constexpr std::float_round_style round_style = std::round_toward_zero;
+    static constexpr bool is_iec559 = false;
+    static constexpr bool is_bounded = true;
+    static constexpr bool is_modulo = true;
+    static constexpr int digits = 128;
+    static constexpr int digits10 = 38;
+    static constexpr int max_digits10 = 0;
+    static constexpr int radix = 2;
+    static constexpr int min_exponent = 0;
+    static constexpr int min_exponent10 = 0;
+    static constexpr int max_exponent = 0;
+    static constexpr int max_exponent10 = 0;
+    static constexpr bool traps = std::numeric_limits<std::uint64_t>::traps;
+    static constexpr bool tinyness_before = false;
+
+    // Member functions
+    static constexpr auto (min)        () -> boost::int128::uint128_t { return {0, 0}; }
+    static constexpr auto lowest       () -> boost::int128::uint128_t { return {0, 0}; }
+    static constexpr auto (max)        () -> boost::int128::uint128_t { return {UINT64_MAX, UINT64_MAX}; }
+    static constexpr auto epsilon      () -> boost::int128::uint128_t { return {0, 0}; }
+    static constexpr auto round_error  () -> boost::int128::uint128_t { return {0, 0}; }
+    static constexpr auto infinity     () -> boost::int128::uint128_t { return {0, 0}; }
+    static constexpr auto quiet_NaN    () -> boost::int128::uint128_t { return {0, 0}; }
+    static constexpr auto signaling_NaN() -> boost::int128::uint128_t { return {0, 0}; }
+    static constexpr auto denorm_min   () -> boost::int128::uint128_t { return {0, 0}; }
+};
+
 #endif //BOOST_INT128_DETAIL_UINT128_HPP
