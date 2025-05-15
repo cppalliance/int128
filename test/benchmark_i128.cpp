2bc0f992c2af8832e04dfcfcf4a7cae4f28026ab
Matt Borland <matt@mattborland.com>
2025-05-15 11:01:15 -0400

    Fix warning for GCC6

--- test/benchmark_i128.cpp
+++ test/benchmark_i128.cpp
@@ -56,12 +56,14 @@ using namespace std::chrono_literals;
 #  pragma GCC diagnostic ignored "-Wold-style-cast"
 #  pragma GCC diagnostic ignored "-Wsign-conversion"
 #  pragma GCC diagnostic ignored "-Wconversion"
-#  pragma GCC diagnostic ignored "-Wduplicated-branches"
 #  pragma GCC diagnostic ignored "-Wunused-parameter"
 #  pragma GCC diagnostic ignored "-Wcast-qual"
 #  define BOOST_INT128_NO_INLINE __attribute__ ((__noinline__))
 #  if __GNUC__ >= 11
 #    pragma GCC diagnostic ignored "-Wstringop-overread"
+#  endif
+#  if __GNUC__ > 6
+#    pragma GCC diagnostic ignored "-Wduplicated-branches"
 #  endif
 #endif
 
