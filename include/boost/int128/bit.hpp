903cd2523ff7308d223a3ed96fe3020cc1c55861
Matt Borland <matt@mattborland.com>
2025-05-16 09:53:05 -0400

    Add bit header and single bit function

---
+++ include/boost/int128/bit.hpp
@@ -0,0 +1,21 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_INT128_BIT_HPP
+#define BOOST_INT128_BIT_HPP
+
+#include <boost/int128/int128.hpp>
+
+namespace boost {
+namespace int128 {
+
+constexpr bool has_single_bit(uint128_t x) noexcept
+{
+    return x && !(x & (--x));
+}
+
+} // namespace int128
+} // namespace boost
+
+#endif // BOOST_INT128_BIT_HPP
