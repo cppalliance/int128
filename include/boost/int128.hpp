64150b9e5b6c52ca8529e48ca8fff10dcbb5b732
Matt Borland <matt@mattborland.com>
2025-05-20 10:50:31 -0400

    Add top level header

---
+++ include/boost/int128.hpp
@@ -0,0 +1,13 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_INT128_HPP
+#define BOOST_INT128_HPP
+
+#include <boost/int128/int128.hpp>
+#include <boost/int128/bit.hpp>
+#include <boost/int128/iostream.hpp>
+#include <boost/int128/literals.hpp>
+
+#endif // BOOST_INT128_HPP
