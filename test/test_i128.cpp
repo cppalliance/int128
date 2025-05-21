e49a2f4791e122643214513beb0b599b64dc602a
Matt Borland <matt@mattborland.com>
2025-05-21 11:26:03 -0400

    Allow sign conversion in testing

--- test/test_i128.cpp
+++ test/test_i128.cpp
@@ -2,6 +2,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#ifndef BOOST_INT128_ALLOW_SIGN_CONVERSION
+#  define BOOST_INT128_ALLOW_SIGN_CONVERSION
+#endif
+
 #include <boost/int128/detail/int128_imp.hpp>
 #include <boost/int128/detail/conversions.hpp>
 #include <boost/core/lightweight_test.hpp>
