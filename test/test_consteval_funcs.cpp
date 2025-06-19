30f1b26c9da230e8e7959efb1a30993ed5788e3e
Matt Borland <matt@mattborland.com>
2025-06-19 14:18:10 -0400

    Remove consteval testing workaround

--- test/test_consteval_funcs.cpp
+++ test/test_consteval_funcs.cpp
@@ -11,35 +11,6 @@
 #  define BOOST_INT128_CONSTEVAL constexpr
 #endif
 
-#if defined(__GNUC__) && __GNUC__ == 7 && defined(__i386__)
-
-/*
-32-bit GCC 7 fails with the following:
-
-libs/decimal/test/test_consteval_funcs.cpp:39:9: error: non-constant condition for static assertion
-static_assert(x * 2U == 4U, "");
-^~~~~~~~~~~~~
-In file included from ./boost/int128/detail/int128_imp.hpp:13:0,
-from ./boost/int128/int128.hpp:9,
-from ./boost/int128.hpp:8,
-from libs/decimal/test/test_consteval_funcs.cpp:6:
-libs/decimal/test/test_consteval_funcs.cpp:39:25:   in constexpr expansion of 'boost::int128::operator*<unsigned int>(x, 2)'
-./boost/int128/detail/uint128_imp.hpp:2276:31:   in constexpr expansion of 'boost::int128::detail::default_mul<long long unsigned int>(lhs, ((uint64_t)rhs))'
-./boost/int128/detail/uint128_imp.hpp:2211:37:   in constexpr expansion of 'boost::int128::detail::knuth_multiply<boost::int128::uint128_t, 4, 2>(((const uint32_t (&)[4])(& lhs_words)), ((const uint32_t (&)[2])(& rhs_words)))'
-./boost/int128/detail/common_mul.hpp:38:9: error: constexpr loop iteration count exceeds limit of 262144 (use -fconstexpr-loop-limit= to increase the limit)
-for (std::size_t i {}; i < u_size; ++i)
-^~~
-
-u_size can only possibly be 1, 2, or 4 so this is clearly incorrect on the compilers side
-*/
-
-int main()
-{
-    return 0;
-}
-
-#else
-
 template <typename T>
 BOOST_INT128_CONSTEVAL bool test()
 {
@@ -102,5 +73,3 @@ int main()
 
     return 0;
 }
-
-#endif // GCC 7 32-bit workaround
