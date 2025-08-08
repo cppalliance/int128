2f24056c1ebb33874be41ba6d510374df50fcb52
Matt Borland <matt@mattborland.com>
2025-08-08 17:57:02 -0400

    Add positive and negative empty format string

--- test/test_format.cpp
+++ test/test_format.cpp
@@ -8,8 +8,32 @@
 
 #ifdef BOOST_INT128_HAS_FORMAT
 
+template <typename T>
+void test_empty()
+{
+    BOOST_TEST_CSTR_EQ(std::format("{}", T{2}).c_str(), "2");
+    BOOST_TEST_CSTR_EQ(std::format("{}", T{22}).c_str(), "22");
+    BOOST_TEST_CSTR_EQ(std::format("{}", T{222}).c_str(), "222");
+    BOOST_TEST_CSTR_EQ(std::format("{}", T{2222}).c_str(), "2222");
+    BOOST_TEST_CSTR_EQ(std::format("{}", T{22222}).c_str(), "22222");
+    BOOST_TEST_CSTR_EQ(std::format("{}", T{222222}).c_str(), "222222");
+
+    if constexpr (std::is_same_v<T, boost::int128::int128_t>)
+    {
+        BOOST_TEST_CSTR_EQ(std::format("{}", T{-2}).c_str(), "-2");
+        BOOST_TEST_CSTR_EQ(std::format("{}", T{-22}).c_str(), "-22");
+        BOOST_TEST_CSTR_EQ(std::format("{}", T{-222}).c_str(), "-222");
+        BOOST_TEST_CSTR_EQ(std::format("{}", T{-2222}).c_str(), "-2222");
+        BOOST_TEST_CSTR_EQ(std::format("{}", T{-22222}).c_str(), "-22222");
+        BOOST_TEST_CSTR_EQ(std::format("{}", T{-222222}).c_str(), "-222222");
+    }
+}
+
 int main()
 {
+    test_empty<boost::int128::uint128_t>();
+    test_empty<boost::int128::int128_t>();
+
     return boost::report_errors();
 }
 
