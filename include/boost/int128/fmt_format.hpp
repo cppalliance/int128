c21e6d0b813d9ccdd5c8b900a411607cf2bb8194
Matt Borland <matt@mattborland.com>
2025-08-15 10:17:19 +0200

    Use respective format_to functions

--- include/boost/int128/fmt_format.hpp
+++ include/boost/int128/fmt_format.hpp
@@ -177,7 +177,6 @@ struct formatter
     template <typename FormatContext>
     auto format(T v, FormatContext& ctx) const
     {
-        auto out {ctx.out()};
         char buffer[64];
         bool isneg {false};
 
@@ -270,7 +269,7 @@ struct formatter
                 // LCOV_EXCL_STOP
         }
 
-        return std::copy(s.begin(), s.end(), out);
+        return fmt::format_to(ctx.out(), "{}", s);
     }
 };
 
