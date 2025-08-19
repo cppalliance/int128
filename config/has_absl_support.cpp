e297be60853f869f3b88671e658e7cddf8adddfa
Matt Borland <matt@mattborland.com>
2025-08-19 16:42:19 +0200

    Add config file

---
+++ config/has_absl_support.cpp
@@ -0,0 +1,11 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <absl/numeric/int128.h>
+
+int main()
+{
+    const absl::uint128 x = 0;
+    return static_cast<int>(x);
+}
