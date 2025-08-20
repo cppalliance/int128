a5bbecc67d7e075cead4fef18e1b65e39164d181
Matt Borland <matt@mattborland.com>
2025-08-20 10:55:25 +0200

    Update config to account for failures on 32-bit multiarch builds

--- config/has_absl_support.cpp
+++ config/has_absl_support.cpp
@@ -2,6 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#ifndef __i386__
+
 #include <absl/numeric/int128.h>
 
 int main()
@@ -9,3 +11,13 @@ int main()
     const absl::uint128 x = 0;
     return static_cast<int>(x);
 }
+
+#else
+
+int main()
+{
+    static_assert(1 == 0, "Does not work on 32-bit linux installed on a 64-bit machine");
+    return 1;
+}
+
+#endif
