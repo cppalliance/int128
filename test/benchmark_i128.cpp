5ece3df6011e8ab561baef52ac38e52a153855b8
Matt Borland <matt@mattborland.com>
2025-08-28 11:46:38 +0200

    Add benchmarking of left shift operation

--- test/benchmark_i128.cpp
+++ test/benchmark_i128.cpp
@@ -319,8 +319,46 @@ BOOST_INT128_NO_INLINE void test_two_element_operation(const std::vector<T>& dat
     std::cerr << operation << "<" << std::left << std::setw(11) << type << ">: " << std::setw( 10 ) << ( t2 - t1 ) / 1us << " us (s=" << s << ")\n";
 }
 
+std::vector<int> generate_shift_vector()
+{
+    std::random_device rd;
+    const auto seed = rd();
+    std::mt19937_64 gen(seed);
+    std::uniform_int_distribution<int> dist(0, 127);
+
+    std::vector<int> data_vec;
+    data_vec.reserve(N);
+    for (std::size_t i {}; i < data_vec.size(); ++i)
+    {
+        data_vec.emplace_back(dist(gen));
+    }
+
+    return data_vec;
+}
+
+template <typename T>
+BOOST_INT128_NO_INLINE void test_left_shift(const std::vector<T>& data_vec, const std::vector<int>& shift_vector, const char* type)
+{
+    const auto t1 = std::chrono::steady_clock::now();
+    std::uint64_t s = 0; // discard variable
+
+    for (std::size_t k {}; k < K; ++k)
+    {
+        for (std::size_t i {}; i < data_vec.size(); ++i)
+        {
+            s += static_cast<std::uint64_t>(data_vec[i] << shift_vector[i]);
+        }
+    }
+
+    const auto t2 = std::chrono::steady_clock::now();
+
+    std::cerr << "ls" << " <" << std::left << std::setw(11) << type << ">: " << std::setw( 10 ) << ( t2 - t1 ) / 1us << " us (s=" << s << ")\n";
+}
+
 int main()
 {
+    const auto shift_vector = generate_shift_vector();
+
     // Two word operations
     {
         std::cerr << "\n---------------------------\n";
@@ -800,6 +838,19 @@ int main()
 
         #ifdef BOOST_INT128_BENCHMARK_ABSL
         test_two_element_operation(absl_vector, std::modulus<>(), "mod", "absl::i128");
+        #endif
+
+        std::cerr << std::endl;
+
+        #if defined(BOOST_INT128_HAS_INT128) || defined(BOOST_INT128_HAS_MSVC_INTERNAL_I128)
+        test_left_shift(builtin_vector, shift_vector, "Builtin");
+        #endif
+
+        test_left_shift(library_vector, shift_vector, "Library");
+        test_left_shift(mp_vector, shift_vector, "mp::i128");
+
+        #ifdef BOOST_INT128_BENCHMARK_ABSL
+        test_left_shift(absl_vector, shift_vector, "absl::i128");
         #endif
 
         std::cerr << std::endl;
