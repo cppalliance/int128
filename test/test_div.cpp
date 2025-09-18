c103e20f0b86040b303b9b03864c1ae6c53fcf3d
Matt Borland <matt@mattborland.com>
2025-09-18 09:01:10 +0200

    Add unsigned div test

---
+++ test/test_div.cpp
@@ -0,0 +1,75 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/int128.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include <random>
+
+#include "boost/multiprecision/fwd.hpp"
+
+using namespace boost::int128;
+
+static constexpr std::size_t N {1024U};
+static std::mt19937_64 rng(42);
+static std::uniform_int_distribution<std::uint64_t> dist(0, UINT64_MAX);
+static std::uniform_int_distribution<std::int64_t> idist(INT64_MIN, INT64_MAX);
+
+#ifdef _MSC_VER
+#  pragma warning(push)
+#  pragma warning(disable:4127)
+#endif
+
+template <std::size_t N>
+void test_unsigned_div()
+{
+    for (std::size_t i {}; i < N; ++i)
+    {
+        uint128_t lhs;
+        uint128_t rhs;
+
+        switch (N)
+        {
+            case 0U:
+                lhs = uint128_t {dist(rng), dist(rng)};
+                rhs = uint128_t {dist(rng), dist(rng)};
+                break;
+            case 1U:
+                lhs = uint128_t {dist(rng)};
+                rhs = uint128_t {dist(rng), dist(rng)};
+                break;
+            case 2U:
+                lhs = uint128_t {dist(rng), dist(rng)};
+                rhs = uint128_t {dist(rng)};
+                break;
+            case 3U:
+                lhs = uint128_t {dist(rng)};
+                rhs = uint128_t {dist(rng)};
+                break;
+            default:                        // LCOV_EXCL_LINE
+                BOOST_INT128_UNREACHABLE;   // LCOV_EXCL_LINE
+        }
+
+        const auto div_res {div(lhs, rhs)};
+        BOOST_TEST_EQ(div_res.quot, lhs / rhs);
+        BOOST_TEST_EQ(div_res.rem, lhs % rhs);
+
+        const auto inv_div_res {div(rhs, lhs)};
+        BOOST_TEST_EQ(inv_div_res.quot, rhs / lhs);
+        BOOST_TEST_EQ(inv_div_res.rem, rhs % lhs);
+    }
+}
+
+#ifdef _MSC_VER
+#  pragma warning(pop)
+#endif
+
+int main()
+{
+    test_unsigned_div<0>();
+    test_unsigned_div<1>();
+    test_unsigned_div<2>();
+    test_unsigned_div<3>();
+
+    return boost::report_errors();
+}
