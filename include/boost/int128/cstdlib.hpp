9e48286ebe1290f814ae36964a8f307534d14697
Matt Borland <matt@mattborland.com>
2025-09-18 10:13:27 +0200

    Add signed div impl

--- include/boost/int128/cstdlib.hpp
+++ include/boost/int128/cstdlib.hpp
@@ -54,6 +54,32 @@ constexpr u128div_t div(const uint128_t x, const uint128_t y) noexcept
     }
 }
 
+constexpr i128div_t div(const int128_t x, const int128_t y) noexcept
+{
+    if (BOOST_INT128_UNLIKELY(x == 0 || y == 0))
+    {
+        return i128div_t{0, 0};
+    }
+
+    const auto abs_lhs {static_cast<uint128_t>(abs(x))};
+    const auto abs_rhs {static_cast<uint128_t>(abs(y))};
+
+    const auto unsigned_res {div(abs_lhs, abs_rhs)};
+
+    #if defined(_MSC_VER) && !defined(__GNUC__)
+    const auto is_neg{static_cast<bool>(x < 0)};
+    #else
+    const auto is_neg {static_cast<bool>((x < 0) != (y < 0))};
+    #endif
+
+    i128div_t res {static_cast<int128_t>(unsigned_res.quot), static_cast<int128_t>(unsigned_res.rem)};
+
+    res.quot = is_neg ? -res.quot : res.quot;
+    res.rem = (x < 0) ? -res.rem : res.rem;
+
+    return res;
+}
+
 } // namespace int128
 } // namespace boost
 
