8ee5f855c03dfcffb767714b4a809c7bf183be59
Matt Borland <matt@mattborland.com>
2026-02-10 08:42:29 -0500

    Make operator bool explicit

--- include/boost/int128/detail/int128_imp.hpp
+++ include/boost/int128/detail/int128_imp.hpp
@@ -98,7 +98,7 @@ int128_t
     #endif // BOOST_INT128_ENDIAN_LITTLE_BYTE
 
     // Integer Conversion operators
-    constexpr operator bool() const noexcept { return low || high; }
+    explicit constexpr operator bool() const noexcept { return low || high; }
 
     template <BOOST_INT128_DEFAULTED_SIGNED_INTEGER_CONCEPT>
     explicit constexpr operator SignedInteger() const noexcept { return static_cast<SignedInteger>(low); }
