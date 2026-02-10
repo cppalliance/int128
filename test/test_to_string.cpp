ade28583b2a46b59e8474619b769122c352b520f
Matt Borland <matt@mattborland.com>
2026-02-10 13:33:17 -0500

    Add additional testing against builtin 8-64 bit integer numeric limits

--- test/test_to_string.cpp
+++ test/test_to_string.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <limits>
 #include <array>
+#include <cstdint>
 
 using namespace boost::int128;
 
@@ -32,10 +33,66 @@ void test()
     }
 }
 
+template <typename T, std::enable_if_t<std::is_signed<T>::value, bool> = true>
+void test_builtin()
+{
+    constexpr T min {std::numeric_limits<T>::min()};
+    constexpr T max {std::numeric_limits<T>::max()};
+    constexpr std::array<T, 4> values = {{ min, max, T{0}, T{42} }};
+
+    for (const auto value : values)
+    {
+        char buffer[64];
+        const auto r {boost::charconv::to_chars(buffer, buffer + sizeof(buffer), value, 10)};
+        BOOST_TEST(r);
+        *r.ptr = '\0';
+
+        const auto value_str {to_string(int128_t{value})};
+
+        BOOST_TEST_CSTR_EQ(buffer, value_str.c_str());
+    }
+}
+
+template <typename T, std::enable_if_t<std::is_unsigned<T>::value, bool> = true>
+void test_builtin()
+{
+    constexpr T min {std::numeric_limits<T>::min()};
+    constexpr T max {std::numeric_limits<T>::max()};
+    constexpr std::array<T, 4> values = {{ min, max, T{0}, T{42} }};
+
+    for (const auto value : values)
+    {
+        char buffer[64];
+        const auto r {boost::charconv::to_chars(buffer, buffer + sizeof(buffer), value, 10)};
+        BOOST_TEST(r);
+        *r.ptr = '\0';
+
+        {
+            const auto value_str {to_string(int128_t{value})};
+
+            BOOST_TEST_CSTR_EQ(buffer, value_str.c_str());
+        }
+        {
+            const auto value_str {to_string(uint128_t{value})};
+
+            BOOST_TEST_CSTR_EQ(buffer, value_str.c_str());
+        }
+    }
+}
+
 int main()
 {
     test<uint128_t>();
     test<int128_t>();
+
+    test_builtin<std::int8_t>();
+    test_builtin<std::uint8_t>();
+    test_builtin<std::int16_t>();
+    test_builtin<std::uint16_t>();
+    test_builtin<std::int32_t>();
+    test_builtin<std::uint32_t>();
+    test_builtin<std::int64_t>();
+    test_builtin<std::uint64_t>();
 
     return boost::report_errors();
 }
