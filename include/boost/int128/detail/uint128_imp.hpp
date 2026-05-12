1293fc094beda96093bf206d4014652239e3a09b
Matt Borland <matt@mattborland.com>
2026-05-12 10:21:38 -0400

    Cross type conversions should be implicit

--- include/boost/int128/detail/uint128_imp.hpp
+++ include/boost/int128/detail/uint128_imp.hpp
@@ -57,8 +57,8 @@ uint128_t
     constexpr uint128_t& operator=(uint128_t&&) noexcept = default;
 
     // Requires a conversion file to be implemented
-    BOOST_INT128_HOST_DEVICE explicit constexpr uint128_t(const int128_t& v) noexcept;
-    BOOST_INT128_HOST_DEVICE explicit constexpr operator int128_t() const noexcept;
+    BOOST_INT128_HOST_DEVICE constexpr uint128_t(const int128_t& v) noexcept;
+    BOOST_INT128_HOST_DEVICE constexpr operator int128_t() const noexcept;
 
     // Construct from integral types
     #if BOOST_INT128_ENDIAN_LITTLE_BYTE
