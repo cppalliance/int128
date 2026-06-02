04fd9f745889d614b010961877299c0344656e82
Matt Borland <matt@mattborland.com>
2026-06-02 15:27:02 -0400

    More UBSAN and ASAN fixes

--- test/test_u128.cpp
+++ test/test_u128.cpp
@@ -67,7 +67,16 @@ T get_root_max()
 template <typename T>
 T get_root_min()
 {
-    return static_cast<T>(std::sqrt(std::numeric_limits<T>::min()));
+    // numeric_limits<T>::min() is negative for signed T, so sqrt() would be NaN
+    // and the cast UB; use the negative of the positive root instead.
+    BOOST_INT128_IF_CONSTEXPR (std::is_signed<T>::value)
+    {
+        return static_cast<T>(-get_root_max<T>());
+    }
+    else
+    {
+        return static_cast<T>(0);
+    }
 }
 
 #include <boost/random/uniform_int_distribution.hpp>
@@ -802,14 +811,9 @@ void test_operator_left_shift()
 
         BOOST_TEST(shifted_emulated == shifted_builtin);
 
-        // Test 3: int128 variants converted to a builtin shift count. shift_value is
-        // bounded to [0, bit-width), so shifting the 128-bit builtin_value is well-defined.
-        const auto builtin_shift = static_cast<unsigned>(static_cast<builtin_u128>(shift_value));
-        const auto emulated_shift = static_cast<unsigned>(boost::int128::uint128_t{shift_value});
-        BOOST_TEST(emulated_shift == builtin_shift);
-
-        auto int_shift_builtin = builtin_value << builtin_shift;
-        auto int_shift_emulated = builtin_value << emulated_shift;
+        // Test 3: Test with IntType as left operand and int128 variants as right operand
+        auto int_shift_builtin = value << static_cast<unsigned>(builtin_value);
+        auto int_shift_emulated = value << static_cast<unsigned>(emulated_value);
 
         static_assert(std::is_same<decltype(int_shift_builtin),
                                    decltype(int_shift_emulated)>::value, "Mismatched types");
@@ -875,14 +879,9 @@ void test_operator_right_shift()
 
         BOOST_TEST(shifted_emulated == shifted_builtin);
 
-        // Test 3: int128 variants converted to a builtin shift count. shift_value is
-        // bounded to [0, bit-width), so shifting the 128-bit builtin_value is well-defined.
-        const auto builtin_shift = static_cast<unsigned>(static_cast<builtin_u128>(shift_value));
-        const auto emulated_shift = static_cast<unsigned>(boost::int128::uint128_t{shift_value});
-        BOOST_TEST(emulated_shift == builtin_shift);
-
-        auto int_shift_builtin = builtin_value >> builtin_shift;
-        auto int_shift_emulated = builtin_value >> emulated_shift;
+        // Test 3: Test with IntType as left operand and int128 variants as right operand
+        auto int_shift_builtin = value >> static_cast<unsigned>(builtin_value);
+        auto int_shift_emulated = value >> static_cast<unsigned>(emulated_value);
 
         static_assert(std::is_same<decltype(int_shift_builtin),
                                    decltype(int_shift_emulated)>::value, "Mismatched types");
