9f2c7c2c429080c903670fe33125f7fbb929d783
Matt Borland <matt@mattborland.com>
2026-06-02 09:15:34 -0400

    Disable test_ckd constexpr for old GCC

--- test/test_ckd.cpp
+++ test/test_ckd.cpp
@@ -479,7 +479,7 @@ void test_mul_edges()
 // constexpr usability for all three operations.
 //
 
-#if defined(__GNUC__) && __GNUC__ == 7 && !defined(__clang__) && !defined(__SIZEOF_INT128__)
+#if defined(__GNUC__) && __GNUC__ <= 7 && !defined(__clang__) && !defined(__SIZEOF_INT128__)
 #  define BOOST_INT128_TEST_CKD_NO_CONSTEXPR_128
 #endif
 
@@ -515,13 +515,13 @@ constexpr int mul_value()
     return r;
 }
 
-#ifndef BOOST_INT128_TEST_CKD_NO_CONSTEXPR_128
 constexpr bool mul_overflows_i128_min()
 {
     int128_t r {0};
     return ckd_mul(&r, (std::numeric_limits<int128_t>::min)(), int128_t{-1});
 }
-#endif
+
+#ifndef BOOST_INT128_TEST_CKD_NO_CONSTEXPR_128
 
 void test_constexpr()
 {
@@ -530,10 +530,10 @@ void test_constexpr()
     static_assert(mul_overflows_int_max(),  "INT_MAX * 2 overflows int");
     static_assert(sub_value() == 2,         "5 - 3 == 2");
     static_assert(mul_value() == 42,        "6 * 7 == 42");
-#ifndef BOOST_INT128_TEST_CKD_NO_CONSTEXPR_128
     static_assert(mul_overflows_i128_min(), "INT128_MIN * -1 overflows int128_t");
-#endif
 }
+
+#endif
 
 int main()
 {
@@ -542,7 +542,10 @@ int main()
     test_add_edges();
     test_sub_edges();
     test_mul_edges();
+
+    #ifndef BOOST_INT128_TEST_CKD_NO_CONSTEXPR_128
     test_constexpr();
+    #endif
 
     return boost::report_errors();
 }
