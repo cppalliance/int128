23951c967d8abe74183dccbf2a33958cbce26d82
Matt Borland <matt@mattborland.com>
2026-07-09 10:32:46 -0400

    Add SYCL testing

---
+++ test/test_signed_to_unsigned_conversion_sycl.cpp
@@ -0,0 +1,14 @@
+//  Copyright 2026 Matt Borland
+//  Distributed under the Boost Software License, Version 1.0.
+//  https://www.boost.org/LICENSE_1_0.txt
+
+#include "sycl_test.hpp"
+
+using boost::int128::int128_t;
+using boost::int128::uint128_t;
+
+int main()
+{
+    return int128_sycl_test::run<int128_t, uint128_t>(
+        [](int128_t a, int128_t, int) { return static_cast<uint128_t>(a); });
+}
---
+++ test/test_unsigned_mul_sycl.cpp
@@ -0,0 +1,13 @@
+//  Copyright 2026 Matt Borland
+//  Distributed under the Boost Software License, Version 1.0.
+//  https://www.boost.org/LICENSE_1_0.txt
+
+#include "sycl_test.hpp"
+
+using boost::int128::uint128_t;
+
+int main()
+{
+    return int128_sycl_test::run<uint128_t, uint128_t>(
+        [](uint128_t a, uint128_t b, int) { return a * b; });
+}
