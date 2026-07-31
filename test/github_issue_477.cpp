19d4770fc7fe0513d2c07b8662bcb552607f9a15
Matt Borland <matt@mattborland.com>
2026-07-31 09:13:34 -0400

    Add reproducer

---
+++ test/github_issue_477.cpp
@@ -0,0 +1,29 @@
+// Copyright 2026 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+//
+// See: https://github.com/cppalliance/int128/issues/477
+
+#include <boost/int128.hpp>
+
+constexpr boost::int128::uint128 value{1, 0};
+
+constexpr bool a = value;
+static_assert(a, "Wrong value");
+
+constexpr bool b(value);
+static_assert(b, "Wrong value");
+
+constexpr bool c = static_cast<bool>(value);
+static_assert(c, "Wrong value");
+
+constexpr boost::int128::int128 signed_value{1, 0};
+
+constexpr bool d = signed_value;
+static_assert(d, "Wrong value");
+
+constexpr bool e(signed_value);
+static_assert(e, "Wrong value");
+
+constexpr bool f = static_cast<bool>(signed_value);
+static_assert(f, "Wrong value");
